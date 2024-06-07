12a6d9944b93e2e9e6cb5fb563794c47acc643c7
Thiago Bianeck <thgbianeck@gmail.com>
2024-06-07 00:33:43 -0300

    str-producer started

---
+++ str-producer/src/main/java/br/com/bianeck/strproducer/StrProducerApplication.java
@@ -0,0 +1,13 @@
+package br.com.bianeck.strproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StrProducerApplication.class, args);
+    }
+
+}
---
+++ str-producer/src/test/java/br/com/bianeck/strproducer/StrProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bianeck.strproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrProducerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
