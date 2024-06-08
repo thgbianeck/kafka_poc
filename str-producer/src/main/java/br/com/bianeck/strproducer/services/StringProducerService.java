60ac1b5cf47e245a64ec97e622e4e3f13529dcc1
Thiago Bianeck <thgbianeck@gmail.com>
2024-06-08 20:56:51 -0300

    Usando a função callback function to sendMessage

---
+++ str-producer/src/main/java/br/com/bianeck/strproducer/services/StringProducerService.java
@@ -0,0 +1,29 @@
+package br.com.bianeck.strproducer.services;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@Log4j2
+@RequiredArgsConstructor
+public class StringProducerService {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public void sendMessage(String message) {
+        kafkaTemplate.send("str-topic", message)
+                .addCallback(
+                        success -> {
+                            if(success != null) {
+                                log.info("Send message with success {}", message);
+                                log.info("Partition {}, Offset {}",
+                                        success.getRecordMetadata().partition(),
+                                        success.getRecordMetadata().offset());
+                            }
+                        },
+                        failure -> log.error("Error send message")
+                );
+    }
+}
