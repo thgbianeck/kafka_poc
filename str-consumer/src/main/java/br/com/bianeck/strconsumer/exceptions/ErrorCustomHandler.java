3ea1363993ababf21c5410846e70f8109c8c1ec9
Thiago Bianeck <thgbianeck@gmail.com>
2024-06-11 09:48:26 -0300

    Error Handler

---
+++ str-consumer/src/main/java/br/com/bianeck/strconsumer/exceptions/ErrorCustomHandler.java
@@ -0,0 +1,24 @@
+package br.com.bianeck.strconsumer.exceptions;
+
+import lombok.extern.log4j.Log4j2;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.springframework.kafka.listener.KafkaListenerErrorHandler;
+import org.springframework.kafka.listener.ListenerExecutionFailedException;
+import org.springframework.messaging.Message;
+import org.springframework.stereotype.Component;
+
+@Log4j2
+@Component
+public class ErrorCustomHandler implements KafkaListenerErrorHandler {
+
+    @Override
+    public Object handleError(Message<?> message, ListenerExecutionFailedException exception) {
+        log.info("EXCEPTION_HANDLER ::: Captured exception");
+        return null;
+    }
+
+//    @Override
+//    public Object handleError(Message<?> message, ListenerExecutionFailedException exception, Consumer<?, ?> consumer) {
+//        return KafkaListenerErrorHandler.super.handleError(message, exception, consumer);
+//    }
+}
