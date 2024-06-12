cc19f8ddb662b93bb747f661b4135278ba22e40a
Thiago Bianeck <thgbianeck@gmail.com>
2024-06-11 23:12:30 -0300

    Add ProducerConfig

--- payment-service/src/main/java/br/com/bianeck/paymentservice/service/impl/PaymentServiceImpl.java
+++ payment-service/src/main/java/br/com/bianeck/paymentservice/service/impl/PaymentServiceImpl.java
@@ -2,15 +2,28 @@ package br.com.bianeck.paymentservice.service.impl;
 
 import br.com.bianeck.paymentservice.model.Payment;
 import br.com.bianeck.paymentservice.service.PaymentService;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+import java.io.Serializable;
+
 @Log4j2
+@RequiredArgsConstructor
 @Service
 public class PaymentServiceImpl implements PaymentService {
 
+    private final KafkaTemplate<String, Serializable> kafkaTemplate;
+
+    @SneakyThrows
     @Override
     public void sendPayment(Payment payment) {
-       log.info("PAYMENT_SERVICE_IMPL ::: Recebi o pagamento: {}", payment);
+       log.info("Recebi o pagamento: {}", payment);
+       Thread.sleep(1000);
+
+       log.info("Enviando pagamento...");
+       kafkaTemplate.send("payment-topic", payment);
     }
 }
